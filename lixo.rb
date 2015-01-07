8643bf94136c8996898152ed7b2cee7ea18107a7
Azul <mail@azulinho.com>
2015-01-07 12:02:56 +0000

    saving files

--- lixo.rb
+++ lixo.rb
@@ -43,6 +43,21 @@ response = RestClient.post 'https://api.terminal.com/v0.1/start_snapshot',
 jenkins = JSON.parse(response)
 ap jenkins
 
+
+
+
+puts "get_snapshot..."
+response = RestClient.post 'https://api.terminal.com/v0.1/get_snapshot',
+  {
+    'user_token' => ENV['TERMINAL_API_TOKEN'],
+    'access_token' => ENV['TERMINAL_ACCESS_TOKEN'],
+    'container_key' => /.*::.*@.*:create:.*::(.*)/.match(jenkins['request_id'])[1],
+  }.to_json,
+  :content_type => :json, :accept => :json
+
+puts JSON.parse(response)
+
+
 puts "start_snapshot..."
 response = RestClient.post 'https://api.terminal.com/v0.1/start_snapshot',
   {
@@ -61,6 +76,27 @@ response = RestClient.post 'https://api.terminal.com/v0.1/start_snapshot',
 
 zabbix = JSON.parse(response)
 ap zabbix
+
+puts "list_terminals..."
+response = RestClient.post 'https://api.terminal.com/v0.1/list_terminals',
+  {
+    'user_token' => ENV['TERMINAL_API_TOKEN'],
+    'access_token' => ENV['TERMINAL_ACCESS_TOKEN']
+  }.to_json,
+  :content_type => :json, :accept => :json
+
+puts JSON.parse(response)
+
+vms = {}
+puts "printing out details from VM"
+JSON.parse(response)['terminals'].each do |vm|
+  ap vm
+  vms["#{vm[name]}"] = vm
+end
+
+puts "printing out vms..."
+ap vms
+
 
 puts "add_terminal_links..."
 response = RestClient.post 'https://api.terminal.com/v0.1/add_terminal_links',
